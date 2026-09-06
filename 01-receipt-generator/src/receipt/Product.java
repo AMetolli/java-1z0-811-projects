fcd16ddd0e897c80219a66b34d9b25572a0547ea
AMetolli <231917075+AMetolli@users.noreply.github.com>
2026-09-06 16:32:02 +0200

    feat(receipt-generator): add Product domain class

---
+++ 01-receipt-generator/src/receipt/Product.java
@@ -0,0 +1,23 @@
+public class Product {
+    private String name;
+    private double price;
+    private TaxCategory taxCategory;
+
+    public Product(String name, double price, TaxCategory taxCategory) {
+        this.name = name;
+        this.price = price;
+        this.taxCategory = taxCategory;
+    }
+
+    public String getName() {
+    return name;
+    }
+
+    public double getPrice() {
+    return price;
+    }
+
+    public TaxCategory getTaxCategory() {
+    return taxCategory;
+    }
+}
